12626e40117b5b42ab13e736dd45f5e045a17e9c
studiohappening17-gif <studiohappening17@gmail.com>
2026-05-26 22:04:15 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
-import { router } from './router'
+import { getRouter } from './router'
 import './styles.css'
+
+const router = getRouter()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
